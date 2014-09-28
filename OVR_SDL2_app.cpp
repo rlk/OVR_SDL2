bab681ef61fe8a8459006333cc68e6731e9b5008
Robert Kooima <robert.kooima@gmail.com>
2014-09-28 13:08:46 -0500

    App documentation

--- OVR_SDL2_app.cpp
+++ OVR_SDL2_app.cpp
@@ -61,10 +61,16 @@ bool OVR_SDL2_app::init_SDL(int x, int y, int w, int h)
     {
         int f = SDL_WINDOW_BORDERLESS | SDL_WINDOW_OPENGL;
 
+        // Choose an OpenGL 3.2 Core Profile. This choice is arbitrary, and
+        // and later version may be substituted.
+
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,
                             SDL_GL_CONTEXT_PROFILE_CORE);
+
+        // Configure 24-bit color and 24-bit depth. This is a common choice,
+        // but may be altered as needed.
 
         SDL_GL_SetAttribute(SDL_GL_RED_SIZE,     8);
         SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE,   8);
@@ -72,12 +78,13 @@ bool OVR_SDL2_app::init_SDL(int x, int y, int w, int h)
         SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE,  24);
         SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+        // Create the window and initialize the OpenGL context.
+
         if ((window = SDL_CreateWindow("OVR_SDL2", x, y, w, h, f)))
         {
             context = SDL_GL_CreateContext(window);
 
             SDL_GameControllerEventState(SDL_ENABLE);
-            SDL_SetRelativeMouseMode(SDL_TRUE);
 
             #ifdef GLEW_VERSION
             glewExperimental = GL_TRUE;
@@ -90,35 +97,40 @@ bool OVR_SDL2_app::init_SDL(int x, int y, int w, int h)
     return false;
 }
 
-/// Initialize the Oculus SDK.
-///
-/// Enable all tracking capability and fall back on a DK1 debug configuration
-/// if no HMD is available.
+/// Initialize the Oculus SDK and return success.
 
 bool OVR_SDL2_app::init_OVR()
 {
     ovr_Initialize();
 
+    // Use the first connected HMD.
+
     hmd = ovrHmd_Create(0);
+
+    // Fall back on a DK1 debug configuration if no HMD is available.
 
     if (hmd == 0)
         hmd = ovrHmd_CreateDebug(ovrHmd_DK1);
+
+    // Enable all tracking capabilities on this HMD.
 
     if (hmd)
         ovrHmd_ConfigureTracking(hmd, ovrTrackingCap_Orientation |
                                       ovrTrackingCap_MagYawCorrection |
                                       ovrTrackingCap_Position, 0);
-    return true;
+
+    return (hmd != 0);
 }
 
-/// Configure the Oculus SDK renderer.
-///
-/// This requires the presence of OpenGL state, so it much be performed AFTER
-/// the SDL window has been created.
+/// Configure the Oculus SDK renderer. This requires the presence of OpenGL
+/// state, so it much be performed AFTER the SDL window has been created.
 
 void OVR_SDL2_app::conf_OVR()
 {
-    // Configure the renderer with all features.
+    // Configure the renderer. Zeroing the configuration stucture causes all
+    // display, window, and device specifications to take on current values
+    // as put in place by SDL. This trick works cross-platform, freeing us
+    // from dealing with any platform issues.
 
     ovrGLConfig cfg;
 
@@ -128,17 +140,22 @@ void OVR_SDL2_app::conf_OVR()
     cfg.OGL.Header.RTSize.w = hmd->Resolution.w;
     cfg.OGL.Header.RTSize.h = hmd->Resolution.h;
 
+    // Set the configuration and receive eye render descriptors in return.
+
     ovrHmd_ConfigureRendering(hmd, &cfg.Config, ovrDistortionCap_Chromatic
                                               | ovrDistortionCap_TimeWarp
                                               | ovrDistortionCap_Overdrive,
                                                 hmd->DefaultEyeFov, erd);
 
-    // Initialize the off-screen render buffers.
+    // Determine the buffer size required by each eye of the current HMD.
 
     ovrSizei size[2];
 
     size[0] = ovrHmd_GetFovTextureSize(hmd, ovrEye_Left,  hmd->DefaultEyeFov[0], 1);
     size[1] = ovrHmd_GetFovTextureSize(hmd, ovrEye_Right, hmd->DefaultEyeFov[1], 1);
+
+    // Initialize the off-screen render buffers. We're using one buffer per-eye
+    // instead of concatenating both eyes into a single buffer.
 
     for (int i = 0; i < 2; i++)
     {
@@ -146,12 +163,12 @@ void OVR_SDL2_app::conf_OVR()
         {
             ovrGLTexture *p = reinterpret_cast<ovrGLTexture*>(tex + i);
 
-            p->OGL.Header.API                   = ovrRenderAPI_OpenGL;
-            p->OGL.Header.TextureSize           = size[i];
-            p->OGL.Header.RenderViewport.Size   = size[i];
-            p->OGL.Header.RenderViewport.Pos.x  = 0;
-            p->OGL.Header.RenderViewport.Pos.y  = 0;
-            p->OGL.TexId                        = buffer[i]->color;
+            memset(p, 0, sizeof (ovrGLTexture));
+
+            p->OGL.Header.API                 = ovrRenderAPI_OpenGL;
+            p->OGL.Header.TextureSize         = size[i];
+            p->OGL.Header.RenderViewport.Size = size[i];
+            p->OGL.TexId                      = buffer[i]->color;
         }
     }
 }
@@ -175,7 +192,10 @@ void OVR_SDL2_app::run()
 
         step();
 
-        // Render both views. Let the Oculus SDK display them on-screen.
+        // Render both views and let the Oculus SDK display them on-screen.
+        // 'eye' is a private member variable that notes which eye is being
+        // rendered. This is used when the application calls back down to
+        // learn the view and projection matrices.
 
         ovrHmd_BeginFrame(hmd, 0);
         {
@@ -198,29 +218,41 @@ void OVR_SDL2_app::dispatch(SDL_Event& e)
     switch (e.type)
     {
         case SDL_KEYDOWN:
-            keyboard(e.key.keysym.scancode, true,  e.key.repeat); break;
+            keyboard(e.key.keysym.scancode, true,  e.key.repeat);
+            break;
         case SDL_KEYUP:
-            keyboard(e.key.keysym.scancode, false, e.key.repeat); break;
+            keyboard(e.key.keysym.scancode, false, e.key.repeat);
+            break;
         case SDL_MOUSEBUTTONDOWN:
-            mouse_button(e.button.button, true);  break;
+            mouse_button(e.button.button, true);
+            break;
         case SDL_MOUSEBUTTONUP:
-            mouse_button(e.button.button, false); break;
+            mouse_button(e.button.button, false);
+            break;
         case SDL_MOUSEMOTION:
-            mouse_motion(e.motion.xrel, e.motion.yrel); break;
+            mouse_motion(e.motion.xrel, e.motion.yrel);
+            break;
         case SDL_MOUSEWHEEL:
-            mouse_wheel(e.wheel.x, e.wheel.y); break;
+            mouse_wheel(e.wheel.x, e.wheel.y);
+            break;
         case SDL_CONTROLLERAXISMOTION:
-            game_axis(e.caxis.which, e.caxis.axis, e.caxis.value / 32768.f); break;
+            game_axis(e.caxis.which, e.caxis.axis, e.caxis.value / 32768.f);
+            break;
         case SDL_CONTROLLERBUTTONDOWN:
-            game_button(e.caxis.which, e.cbutton.button, true);  break;
+            game_button(e.caxis.which, e.cbutton.button, true);
+            break;
         case SDL_CONTROLLERBUTTONUP:
-            game_button(e.caxis.which, e.cbutton.button, false); break;
+            game_button(e.caxis.which, e.cbutton.button, false);
+            break;
         case SDL_CONTROLLERDEVICEADDED:
-            game_connect(e.cdevice.which, true);  break;
+            game_connect(e.cdevice.which, true);
+            break;
         case SDL_CONTROLLERDEVICEREMOVED:
-            game_connect(e.cdevice.which, false); break;
+            game_connect(e.cdevice.which, false);
+            break;
         case SDL_QUIT:
-            running = false; break;
+            running = false;
+            break;
     }
 }
 
@@ -295,32 +327,42 @@ static mat4 getMatrix4f(const OVR::Matrix4f& m)
                 m.M[3][0], m.M[3][1], m.M[3][2], m.M[3][3]);
 }
 
-/// Return the current projection matrix.
+/// Return the current projection matrix as determined by the current OVR eye
+/// render descriptor.
 
 mat4 OVR_SDL2_app::projection() const
 {
     return getMatrix4f(ovrMatrix4f_Projection(erd[eye].Fov, 0.1f, 100.0f, true));
 }
 
-/// Return the current view matrix.
+/// Return the current view matrix as determined by the current OVR eye render
+/// descriptor and head pose.
 
 mat4 OVR_SDL2_app::view() const
 {
-    OVR::Quatf q = OVR::Quatf(pose[eye].Orientation);
+    // Orientation of the head
 
+    OVR::Quatf q = OVR::Quatf(pose[eye].Orientation);
     mat4 O = getMatrix4f(OVR::Matrix4f(q.Inverted()));
+
+    // Offset of the eye from the center of the head
 
     mat4 E = translation(vec3(erd[eye].ViewAdjust.x,
                               erd[eye].ViewAdjust.y,
                               erd[eye].ViewAdjust.z));
 
-    mat4 H = translation(vec3(-pose[eye].Position.x,
+    // Offset of the head from the center of the world
+
+    mat4 P = translation(vec3(-pose[eye].Position.x,
                               -pose[eye].Position.y,
                               -pose[eye].Position.z));
-    return E * O * H;
+
+    return E * O * P;
 }
 
 //------------------------------------------------------------------------------
+
+/// Check if the OVR health & safety warning is visible and try to dismiss it.
 
 void OVR_SDL2_app::dismiss_warning()
 {
